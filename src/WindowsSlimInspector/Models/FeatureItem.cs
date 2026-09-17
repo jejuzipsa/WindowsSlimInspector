595b31b301d21c0c1b44775979b6e63f9504b950
jejuzipsa <demwj01@gmail.com>
2026-09-17 18:06:46 +0900

    Add feature state model

---
+++ src/WindowsSlimInspector/Models/FeatureItem.cs
@@ -0,0 +1,71 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Media;
+
+namespace WindowsSlimInspector.Models;
+
+public enum FeatureState
+{
+    Unknown,
+    Enabled,
+    Disabled,
+    Unsupported,
+    Error
+}
+
+public sealed class FeatureItem : INotifyPropertyChanged
+{
+    private bool _isSelected;
+    private FeatureState _state = FeatureState.Unknown;
+    private string _detail = string.Empty;
+
+    public required string Id { get; init; }
+    public required string Name { get; init; }
+    public required string Description { get; init; }
+
+    public bool IsSelected
+    {
+        get => _isSelected;
+        set { _isSelected = value; OnPropertyChanged(); }
+    }
+
+    public FeatureState State
+    {
+        get => _state;
+        set
+        {
+            _state = value;
+            OnPropertyChanged();
+            OnPropertyChanged(nameof(StateText));
+            OnPropertyChanged(nameof(StateBrush));
+        }
+    }
+
+    public string Detail
+    {
+        get => _detail;
+        set { _detail = value; OnPropertyChanged(); }
+    }
+
+    public string StateText => State switch
+    {
+        FeatureState.Enabled => "켜짐",
+        FeatureState.Disabled => "꺼짐",
+        FeatureState.Unsupported => "지원 안 됨",
+        FeatureState.Error => "확인 실패",
+        _ => "확인 중"
+    };
+
+    public Brush StateBrush => State switch
+    {
+        FeatureState.Disabled => Brushes.IndianRed,
+        FeatureState.Enabled => Brushes.ForestGreen,
+        FeatureState.Unsupported => Brushes.DarkGray,
+        FeatureState.Error => Brushes.DarkOrange,
+        _ => Brushes.SlateGray
+    };
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+    private void OnPropertyChanged([CallerMemberName] string? name = null) =>
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+}
